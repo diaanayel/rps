daa20f39e6d272924c723e7bd2cc7b9d23194ce8
diaanayel <diaanayel297@gmail.com>
2025-02-19 19:54:49 +0200

    add logic
    now we have a working game, shitty, yeah, but working, yay

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+const values = ["rock", "paper", "scissors"];
+
+function getPCChoice() {
+  let choice = Math.floor(Math.random() * 3);
+  return values[choice];
+}
+
+function play(userChoice) {
+  const pcChoice = getPCChoice();
+  let winner = "draw";
+
+  if(userChoice === pcChoice) {}
+  else
+    winner = (userChoice === "rock" && pcChoice === "scissors"
+      || userChoice === "scissors" && pcChoice === "paper"
+      || userChoice === "paper" && pcChoice === "rock")
+      ? "you" : "pc";
+
+  document.getElementById("result").innerText = (message(userChoice, pcChoice, winner));
+}
+
+function message(userChoice, pcChoice, winner) {
+  return "you: " + userChoice + ", pc: " + pcChoice + " -> " + winner;
+}
