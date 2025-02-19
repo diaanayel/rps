6470154628f6dfa249f1a9945c7c0b2d071f10cb
diaanayel <diaanayel297@gmail.com>
2025-02-19 20:24:00 +0200

    change message fn to dislay and extract compare fn
    displaying results is now delegated to its own fn, comparison was inside
    play fn but now it's extracted

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,23 @@
 const choices = ["rock", "paper", "scissors"];
 
-function getPCChoice() {
-  return Math.floor(Math.random() * 3);
-}
+function getPCChoice() { return Math.floor(Math.random() * 3); }
 
 function play(userChoice) {
   const pcChoice = getPCChoice();
   userChoice = choices.indexOf(userChoice);
-  let winner = "draw";
-
-  if(userChoice === pcChoice) {}
-  else
-    winner = (userChoice === 0 && pcChoice === 2
-      || userChoice === 2 && pcChoice === 1
-      || userChoice === 1 && pcChoice === 0) ? "you" : "pc";
-
-  document.getElementById("result").innerText = (message(choices[userChoice], choices[pcChoice], winner));
+  display(choices[userChoice], choices[pcChoice], winnerOrDraw(userChoice, pcChoice));
 }
 
-function message(userChoice, pcChoice, winner) {
-  return "you: " + userChoice + ", pc: " + pcChoice + " -> " + winner;
+function winnerOrDraw(userChoice, pcChoice) {
+  return (userChoice === pcChoice) ? "draw" : getWinner(userChoice, pcChoice);
+}
+
+function getWinner(userChoice, pcChoice) {
+  return (userChoice === 0 && pcChoice === 2
+    || userChoice === 2 && pcChoice === 1
+    || userChoice === 1 && pcChoice === 0) ? "you" : "pc";
+}
+
+function display(userChoice, pcChoice, winner) {
+  document.getElementById("result").innerText = ("you: " + userChoice + ", pc: " + pcChoice + " -> " + winner);
 }
