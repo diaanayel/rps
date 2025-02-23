a763d09c96ce19869362d3448b10ff63b5d271bf
diaanayel <diaanayel297@gmail.com>
2025-02-23 19:17:46 +0200

    add new UI and scoring system

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Foundation - Rock Paper Scrissors Game
 
 [Lesson on TOP](https://www.theodinproject.com/lessons/foundations-rock-paper-scissors)
+
+## Third-Party Assets
--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,117 @@
-const choices = ["rock", "paper", "scissors"];
+/*
+I know polluting env with global vars is forbidden, may god forgive me :(
+The UI is not responsive cuz my laptop is broken and i made this with the phone
 
-document.querySelector(".controls").addEventListener("click", (e) => {
-  const pcChoiceIdx   = getPCChoice();
-  const userChoiceIdx = choices.indexOf(e.target.value);
-  if(userChoiceIdx === -1) return;
+TODO
+refactor!.. this is also forbidden :(
+*/
 
-  display(choices[userChoiceIdx], choices[pcChoiceIdx],
-    winnerOrDraw(userChoiceIdx, pcChoiceIdx));
+const userFigure = document.querySelector("#user-figure");
+const pcFigure   = document.querySelector("#pc-figure");
+
+const userChoiceImg  = document.querySelector("#user-choice-img");
+const pcChoiceImg    = document.querySelector("#pc-choice-img");
+const userScoreLabel = document.querySelector("#user-score");
+const pcScoreLabel   = document.querySelector("#pc-score");
+const winnerLabel    = document.querySelector("#winner");
+
+const choices = {
+  rock: { value: 0, name: "rock", img: "/src/assets/svgs/rock.svg" },
+  paper: { value: 1, name: "paper", img: "/src/assets/svgs/paper.svg" },
+  scissors: { value: 2, name: "scissors", img: "/src/assets/svgs/scissors.svg" },
+  fresh: { value: 3, name: "fresh", img: "/src/assets/svgs/qmark.svg" }
+};
+
+const user = { name: "You", score: 0, latestChoice: null };
+const pc = { name: "Computer", score: 0, latestChoice: null };
+
+let hasGameEnded = false;
+let gameWinner = null;
+
+document.querySelector("#new-game").addEventListener("click", () => {
+  user.latestChoice = choices.fresh;
+  pc.latestChoice   = choices.fresh;
+  user.score = 0;
+  pc.score   = 0;
+  winnerLabel.innerText = "";
+  gameWinner = null;
+  hasGameEnded = false;
+
+  updateUI();
 });
 
-function winnerOrDraw(userChoice, pcChoice) {
-  return (userChoice === pcChoice) ? "draw" : getWinner(userChoice, pcChoice);
+document.querySelector(".choices").addEventListener("click", (e) => {
+  if(hasGameEnded) return;
+
+  pc.latestChoice   = Object.values(choices)[getPCChoiceIdx()];
+  user.latestChoice = choices[e.target.alt];
+  if(user.latestChoice === null) return;
+
+  let roundWinner = null;
+
+  if(!(isDraw())) {
+    roundWinner = getRoundWinner();
+    roundWinner.score++;
+  }
+
+  updateUI(roundWinner);
+
+  if(isGameOver()) {
+    gameWinner = getGameWinner();
+    displayGamewinner();
+    hasGameEnded = true;
+  }
+});
+
+function isGameOver() {
+  return (user.score === 5 || pc.score === 5) ? true : false;
 }
 
-function getWinner(userChoice, pcChoice) {
-  return (isUserWinner(userChoice, pcChoice)) ? "you" : "pc";
+function getGameWinner() {
+  return (user.score === 5) ? user : pc;
 }
 
-function isUserWinner(userChoice, pcChoice) {
-  const ROCK_VS_SCISSORS  = userChoice === 0 && pcChoice === 2;
-  const SCISSORS_VS_PAPER = userChoice === 2 && pcChoice === 1;
-  const PAPER_VS_ROCK     = userChoice === 1 && pcChoice === 0;
+function getRoundWinner() {
+  return isUserWinner(user.latestChoice.value, pc.latestChoice.value) ? user : pc;
+}
+
+function isDraw() {
+  return (user.latestChoice.value === pc.latestChoice.value)
+}
+
+function isUserWinner(userChoiceValue, pcChoiceValue) {
+  const ROCK_VS_SCISSORS  = userChoiceValue === 0 && pcChoiceValue === 2;
+  const SCISSORS_VS_PAPER = userChoiceValue === 2 && pcChoiceValue === 1;
+  const PAPER_VS_ROCK     = userChoiceValue === 1 && pcChoiceValue === 0;
 
   return ROCK_VS_SCISSORS || SCISSORS_VS_PAPER || PAPER_VS_ROCK;
 }
 
-function getPCChoice() { return Math.floor(Math.random() * 3); }
+function getPCChoiceIdx() { return Math.floor(Math.random() * 3); }
 
-function display(userChoice, pcChoice, winner) {
-  document.getElementById("result").innerText = ("you: " + userChoice + ", pc: " + pcChoice + " -> " + winner);
+function displayGamewinner() {
+  winnerLabel.innerText = gameWinner.name + " won";
+}
+
+function updateUI(winner) {
+  userChoiceImg.src = user.latestChoice.img;
+  pcChoiceImg.src   = pc.latestChoice.img;
+
+  userScoreLabel.innerText = user.score;
+  pcScoreLabel.innerText = pc.score;
+
+  if (winner === user) {
+    userFigure.classList.add("winner");
+    pcFigure.classList.add("loser");
+    userFigure.classList.remove("loser");
+    pcFigure.classList.remove("winner");
+  } else if (winner === pc) {
+    userFigure.classList.add("loser");
+    pcFigure.classList.add("winner");
+    userFigure.classList.remove("winner");
+    pcFigure.classList.remove("loser");
+  } else {
+    userFigure.classList.remove("winner", "loser");
+    pcFigure.classList.remove("winner", "loser");
+  }
 }
